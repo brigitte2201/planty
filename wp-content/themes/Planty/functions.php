580e54b130f781c3db1362a458ad9c8353177f0f
brigitte bellard <brigitte.bellard@buenavistasocialcom.fr>
2024-01-14 12:07:08 +0100

    Responsive formulaire commande-planty

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -7,8 +7,6 @@ function ns_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme-planty.css', array(), filemtime(get_stylesheet_directory() . '/css/theme-planty.css'));
     wp_enqueue_style('form-style', get_stylesheet_directory_uri() . '/css/form-planty.css', array(), filemtime(get_stylesheet_directory() . '/css/form-planty.css'));
-   
-    
 }
 
 add_action( 'wp_enqueue_scripts', 'ns_enqueue_styles' );
